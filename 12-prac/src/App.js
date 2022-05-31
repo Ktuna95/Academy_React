141a5624ba168a6f9ed1c9cf3045d81877b492d9
Ktuna <hakunamatata95@naver.com>
2022-05-31 22:01:35 +0900

    DESKTOP

--- 12-prac/src/App.js
+++ 12-prac/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { memo } from 'react';
+import {Routes, Route} from "react-router-dom";
+import MenuLink from './components/MenuLink';
 
-const App = () => {
+import  News from './pages/News';
+import TrafficAcc from './pages/TrafficAcc';
+
+const App = memo(() => {
   return (
-    <div></div>
+    <div>
+      <h1>Exam 12, 13</h1>
+      <nav>
+        <MenuLink to ="/news">News</MenuLink>
+        <MenuLink to ="/traffic_acc">Traffic Acc</MenuLink>
+      </nav>
+
+      <Routes>
+        <Route path="/news" element={<News />} />
+        <Route path="/traffic_acc" element={<TrafficAcc />} />
+      </Routes>
+    </div>
   );
-}
+});
 
 export default App;
--- 12-prac/src/index.js
+++ 12-prac/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRrouter} from 'react-router-dom';
+
+/**리덕스 구성을 위한 참조 */
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRrouter>
+        <App />
+      </BrowserRrouter>
+    </Provider>
   </React.StrictMode>
 );
